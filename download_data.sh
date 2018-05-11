9d1f2b330f2dd4819239b30b50488a0dfccac8d4
Shane <shanemcq18@gmail.com>
2018-05-11 11:11:15 -0600

    fix .gitignore, revise download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,7 @@
 # Download data files and place them in the lab folders.
 
 SOURCE="https://github.com/Foundations-of-Applied-Mathematics/Data.git"
+LABS=("Conditioning_Stability" "DataVisualization" "Differentiation" "DrazinInverse" "Exceptions_FileIO" "FacialRecognition" "ImageSegmentation" "LeastSquares_Eigenvalues" "LinearTransformations" "MatplotlibIntro" "NumpyIntro" "PageRank" "SVD_ImageCompression")
 GIT="https://git-scm.com"
 TEMPDIR="_DATA_"`date +%s`"_"
 
@@ -9,46 +10,30 @@ TEMPDIR="_DATA_"`date +%s`"_"
 command -v git > /dev/null ||
 { echo -e "\nERROR: git is required. Download it at $GIT.\n"; exit 1; }
 
-# Download the data using git sparse checkout and git lfs.
+# Download the data using git sparse checkout.
 mkdir ${TEMPDIR}
 cd ${TEMPDIR}
 git init --quiet
 echo -e "\nInitializing Download ...\n"
 git remote add -f origin "${SOURCE}"
 git config core.sparseCheckout true
-echo "Conditioning_Stability" >> .git/info/sparse-checkout
-echo "DataVisualization" >> .git/info/sparse-checkout
-echo "Differentiation" >> .git/info/sparse-checkout
-echo "DrazinInverse" >> .git/info/sparse-checkout
-echo "Exceptions_FileIO" >> .git/info/sparse-checkout
-echo "FacialRecognition" >> .git/info/sparse-checkout
-echo "ImageSegmentation" >> .git/info/sparse-checkout
-echo "LeastSquares_Eigenvalues" >> .git/info/sparse-checkout
-echo "LinearTransformations" >> .git/info/sparse-checkout
-echo "MatplotlibIntro" >> .git/info/sparse-checkout
-echo "NumpyIntro" >> .git/info/sparse-checkout
-echo "PageRank" >> .git/info/sparse-checkout
-echo "SVD_ImageCompression" >> .git/info/sparse-checkout
+for lab in ${LABS[@]}; do
+    echo "${lab}" >> .git/info/sparse-checkout
+done
 git pull origin master
 cd ../
 
 # Migrate the files from the temporary folder.
 set +e
 echo -e "\nMigrating files ..."
-
-cp -v ${TEMPDIR}/Conditioning_Stability/* ./Conditioning_Stability/
-cp -v ${TEMPDIR}/DataVisualization/* ./DataVisualization/
-cp -v ${TEMPDIR}/Differentiation/* ./Differentiation/
-cp -v ${TEMPDIR}/DrazinInverse/* ./DrazinInverse/
-cp -v ${TEMPDIR}/Exceptions_FileIO/* ./Exceptions_FileIO/
-cp -v ${TEMPDIR}/FacialRecognition/* ./FacialRecognition/
-cp -v ${TEMPDIR}/ImageSegmentation/* ./ImageSegmentation/
-cp -v ${TEMPDIR}/LeastSquares_Eigenvalues/* ./LeastSquares_Eigenvalues/
-cp -v ${TEMPDIR}/LinearTransformations/* ./LinearTransformations/
-cp -v ${TEMPDIR}/MatplotlibIntro/* ./MatplotlibIntro/
-cp -v ${TEMPDIR}/NumpyIntro/* ./NumpyIntro/
-cp -v ${TEMPDIR}/PageRank/* ./PageRank/
-cp -v ${TEMPDIR}/SVD_ImageCompression/* ./SVD_ImageCompression/
+for lab in ${LABS[@]}; do
+    # Check that the target directory exists before copying.
+    if [ -d "${lab}" ]; then
+        cp -v ${TEMPDIR}/${lab}/* ./${lab}/
+    else
+        echo -e "\nERROR: directory '${lab}' not found!\n"
+    fi
+done
 
 # Delete the temporary folder.
 rm -rf ${TEMPDIR}
